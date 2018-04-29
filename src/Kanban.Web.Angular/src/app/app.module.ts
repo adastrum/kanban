5795e596b9567ac8b4ac1b4b3bd1fc5af29cdf25
ad_astrum <trofimovde@outlook.com>
2018-04-29 21:49:10 +0300

    Add ts clients for task, users

--- src/Kanban.Web.Angular/src/app/app.module.ts
+++ src/Kanban.Web.Angular/src/app/app.module.ts
@@ -5,23 +5,26 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { ProjectsComponent } from "./components/project/projects.component";
-import { ProjectsClient, API_BASE_URL } from "./clients/project.client";
+import { ProjectsClient } from "./clients/project.client";
+import { ProjectTasksClient } from "./clients/project.task.client";
 import { AppRoutingModule } from "./app-routing.module";
 import { ProjectComponent } from "./components/project/project.component";
 import { ProjectClientMock } from "./clients/project.client.mock";
+import { API_BASE_URL } from "./globals";
 
 @NgModule({
   declarations: [AppComponent, ProjectsComponent, ProjectComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
   providers: [
-    { provide: ProjectsClient, useClass: ProjectClientMock },
-    // ProjectsClient,
+    // { provide: ProjectsClient, useClass: ProjectClientMock },
+    ProjectsClient,
+    ProjectTasksClient,
     {
       provide: API_BASE_URL,
-      useValue: "http://kanban.api"
-      // useValue: "http://localhost:55146"
+      // useValue: "http://kanban.api"
+      useValue: "http://localhost:55146"
     }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
